c80174b95a910b1091e94eee6890f5dfe6dda106
James Pearce <github@tripleodeon.com>
2025-04-06 16:25:35 -0930

    [tests] Working React tests

--- test/unit/core/ui-react/hooks.test.tsx
+++ test/unit/core/ui-react/hooks.test.tsx
@@ -1,6 +1,7 @@
 import {render} from '@testing-library/react';
-import React from 'react';
-import {ManagerProvider} from 'tinytick/ui-react';
+import React, {act, StrictMode} from 'react';
+import {ManagerProvider, useManager} from 'tinytick/ui-react';
+import {pause} from '../../common.ts';
 
 let didRender: jest.Mock;
 beforeEach(() => {
@@ -8,17 +9,127 @@ beforeEach(() => {
 });
 
 describe('Context Hooks', () => {
-  test('useStore', () => {
-    const Test = () => didRender(<>hi</>);
+  describe('useManager', () => {
+    test('basic', () => {
+      const Test = () =>
+        didRender(useManager()?.getManagerConfig(true).tickInterval);
 
-    const {container, unmount} = render(
-      <ManagerProvider>
-        <Test />
-      </ManagerProvider>,
-    );
-    expect(container.textContent).toEqual(JSON.stringify({t1: {r1: {c1: 1}}}));
-    expect(didRender).toHaveBeenCalledTimes(1);
+      const {container, unmount} = render(
+        <ManagerProvider>
+          <Test />
+        </ManagerProvider>,
+      );
+      expect(didRender).toHaveBeenCalledTimes(2);
+      expect(container.textContent).toEqual('100');
 
-    unmount();
+      unmount();
+    });
+
+    test('started', async () => {
+      let count = 0;
+      const Test = () => {
+        const manager = useManager();
+        manager?.setTask('count', async () => count++);
+        manager?.scheduleTaskRun('count', '', 0);
+        return didRender(
+          <>
+            {manager?.getStatus()},{manager?.getScheduledTaskRunIds().length}
+          </>,
+        );
+      };
+
+      const {container, unmount} = render(
+        <ManagerProvider>
+          <Test />
+        </ManagerProvider>,
+      );
+      expect(didRender).toHaveBeenCalledTimes(2);
+      expect(container.textContent).toEqual('1,1');
+      expect(count).toEqual(0);
+
+      await act(async () => await pause(100));
+      expect(count).toEqual(1);
+
+      unmount();
+    });
+
+    test('stopped', async () => {
+      let count = 0;
+      const Test = () => {
+        const manager = useManager();
+        manager?.setTask('count', async () => count++);
+        manager?.scheduleTaskRun('count', '', 0);
+        return didRender(
+          <>
+            {manager?.getStatus()},{manager?.getScheduledTaskRunIds().length}
+          </>,
+        );
+      };
+
+      const {container, unmount} = render(
+        <ManagerProvider started={false}>
+          <Test />
+        </ManagerProvider>,
+      );
+      expect(didRender).toHaveBeenCalledTimes(2);
+      expect(container.textContent).toEqual('0,1');
+      expect(count).toEqual(0);
+
+      await act(async () => await pause(100));
+      expect(count).toEqual(0);
+
+      unmount();
+    });
+
+    test('strict mode', async () => {
+      let count = 0;
+      const Test = () => {
+        const manager = useManager();
+        manager?.setTask('count', async () => count++);
+        manager?.scheduleTaskRun('count', '', 0);
+        return didRender(manager?.getScheduledTaskRunIds().length);
+      };
+
+      const {container, unmount} = render(
+        <StrictMode>
+          <ManagerProvider>
+            <Test />
+          </ManagerProvider>
+        </StrictMode>,
+      );
+
+      expect(didRender).toHaveBeenCalledTimes(4);
+      expect(container.textContent).toEqual('2');
+      expect(count).toEqual(0);
+
+      await act(async () => await pause(100));
+      expect(count).toEqual(2);
+
+      unmount();
+    });
+
+    test('unmount', async () => {
+      let count = 0;
+      const Test = () => {
+        const manager = useManager();
+        manager?.setTask('count', async () => count++);
+        manager?.scheduleTaskRun('count', '', 0);
+        return didRender(manager?.getScheduledTaskRunIds().length);
+      };
+
+      const {container, unmount} = render(
+        <ManagerProvider>
+          <Test />
+        </ManagerProvider>,
+      );
+
+      expect(didRender).toHaveBeenCalledTimes(2);
+      expect(container.textContent).toEqual('1');
+      expect(count).toEqual(0);
+
+      unmount();
+      await act(async () => await pause(100));
+      expect(count).toEqual(0);
+    });
   });
 });
